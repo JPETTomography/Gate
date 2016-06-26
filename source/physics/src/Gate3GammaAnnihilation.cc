d45e2e975f8a24560d7f0e713598cb49e9eb21f8
Nikodem Krawczyk <nikodem.krawczyk@gmail.com>
2016-06-26 16:50:11 +0200

    added JPETActor

--- source/physics/src/Gate3GammaAnnihilation.cc
+++ source/physics/src/Gate3GammaAnnihilation.cc
@@ -31,7 +31,7 @@ Gate3GammaAnnihilation::~Gate3GammaAnnihilation()
 //-------------------------------------------------------------------------------------------------
 void Gate3GammaAnnihilation::Initialize()
 {
-  m_source->SetNumberOfParticles( 1 );
+  m_source->SetNumberOfParticles( 3 );
   m_source->SetParticleTime( m_source->GetTime() );
 }
 //-------------------------------------------------------------------------------------------------
@@ -43,42 +43,58 @@ void Gate3GammaAnnihilation::GenerateVertex( G4Event* aEvent)
 
   m_source->GeneratePrimaryVertex( aEvent );
   G4PrimaryParticle* particle1 = aEvent->GetPrimaryVertex( 0 )->GetPrimary( 0 );
-  // G4PrimaryParticle* particle2 = aEvent->GetPrimaryVertex( 0 )->GetPrimary( 1 );
-  // G4PrimaryParticle* particle3 = aEvent->GetPrimaryVertex( 0 )->GetPrimary( 2 );gammaMom.y()*gammaMom.y() + gammaMom.z()*gammaMom.z()
+  G4PrimaryParticle* particle2 = aEvent->GetPrimaryVertex( 0 )->GetPrimary( 1 );
+  G4PrimaryParticle* particle3 = aEvent->GetPrimaryVertex( 0 )->GetPrimary( 2 );
 
-    Double_t mass_e = 511.0 ; //keV
-    TRandom3 *  random_generator = new TRandom3();
 
+
+/*===================================================================
+=            Begining of code provided by Daria Kamińska            =
+===================================================================*/
+
+
+  Double_t mass_e = 511.0 ; //keV
+  TRandom3 *  random_generator = new TRandom3();
+
+  // informacje o pozytonium
   TLorentzVector vec_pozytonium(0.0, 0.0, 0.0, 2.0*mass_e);
 
   // masy cząstek potomnych =0 -> gammy
   Double_t mass_secondaries[3] = {0.0, 0.0, 0.0};
 
+  // TGenPhaseSpace - Utility class to generate n-body event
   TGenPhaseSpace event;
   event.SetDecay(vec_pozytonium, 3, mass_secondaries);
 
+  // uwzględnienie wag rozpadu
   Double_t weight;
   Double_t weight_max= event.GetWtMax()*pow(10,5);
   Double_t rwt;
   Double_t M_max = 7.65928*pow(10,-6);
 
 
-        do{
-            weight = event.Generate();
-            weight = weight*pow((mass_e-event.GetDecay(0)->E())/(event.GetDecay(1)->E()*event.GetDecay(2)->E()),2) + pow((mass_e-event.GetDecay(1)->E())/(event.GetDecay(0)->E()*event.GetDecay(2)->E()),2) + pow((mass_e-event.GetDecay(2)->E())/(event.GetDecay(0)->E()*event.GetDecay(1)->E()),2);
-            rwt = random_generator->Uniform(M_max*weight_max);
-        }while( rwt > weight );
+    do{
+        weight = event.Generate();
+        weight = weight*pow((mass_e-event.GetDecay(0)->E())/(event.GetDecay(1)->E()*event.GetDecay(2)->E()),2) + pow((mass_e-event.GetDecay(1)->E())/(event.GetDecay(0)->E()*event.GetDecay(2)->E()),2) + pow((mass_e-event.GetDecay(2)->E())/(event.GetDecay(0)->E()*event.GetDecay(1)->E()),2);
+        rwt = random_generator->Uniform(M_max*weight_max);
+    }while( rwt > weight );
 
-        TLorentzVector * g1 = event.GetDecay(0);
-        TLorentzVector * g2 = event.GetDecay(1);
-        TLorentzVector * g3 = event.GetDecay(2);
+    // get momenta
+    TLorentzVector * g1 = event.GetDecay(0);
+    TLorentzVector * g2 = event.GetDecay(1);
+    TLorentzVector * g3 = event.GetDecay(2);
 
 
-  G4ThreeVector gammaMom = particle1->GetMomentum();
-  particle1 -> SetMomentum( g1->P(), 0, 0 );
-  // particle1->SetMomentum( gammaMom.x(), 0, 0 );
-  // particle2->SetMomentum( g2->Px()*g2->Px() + g2->Py()*g2->Py() + g2->Pz()*g2->Pz(),0, 0 );
-  // particle3->SetMomentum( 0, 0, gammaMom.z() );
+/*==============================================================
+=            End of code provided by Daria Kamińska            =
+==============================================================*/
+
+
+
+
+  particle1->SetMomentum( g1 -> Px(), g1 -> Py(), g1 -> Pz() );
+  particle2->SetMomentum( g2 -> Px(), g2 -> Py(), g2 -> Pz() );
+  particle3->SetMomentum( g3 -> Px(), g3 -> Py(), g3 -> Pz() );
 
 
 
