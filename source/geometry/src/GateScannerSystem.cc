5e84bc0289a56c36916820e47ba0386050bb992b
Nikodem Krawczyk <nikodem.krawczyk@gmail.com>
2016-10-24 14:05:26 +0200

    Seed taken from gRandom for creation of 3 gamma decays

--- source/geometry/src/GateScannerSystem.cc
+++ source/geometry/src/GateScannerSystem.cc
@@ -21,27 +21,27 @@ GateScannerSystem::GateScannerSystem(const G4String& itsName)
   // Setup a messenger
   m_messenger = new GateClockDependentMessenger(this);
   m_messenger->SetDirectoryGuidance(G4String("Controls the system '") + GetObjectName() + "'" );
-  
+
   // Define the scanner components
   GateSystemComponent* aComponent;
   aComponent = new GateSystemComponent("level1",GetBaseComponent(),this);
   aComponent = new GateSystemComponent("level2",aComponent,this);
   aComponent = new GateSystemComponent("level3",aComponent,this);
   aComponent = new GateSystemComponent("level4",aComponent,this);
-/*  aComponent = new GateSystemComponent("level5",aComponent,this);*/
+  aComponent = new GateSystemComponent("level5",aComponent,this);
   new GateSystemComponent("layer0",aComponent,this);
   new GateSystemComponent("layer1",aComponent,this);
   SetOutputIDName((char *)"level1ID",1);
   SetOutputIDName((char *)"level2ID",2);
   SetOutputIDName((char *)"level3ID",3);
   SetOutputIDName((char *)"level4ID",4);
-/*  SetOutputIDName((char *)"level5ID",5);*/
+  SetOutputIDName((char *)"level5ID",5);
   SetOutputIDName((char *)"layerID",5);
 }
 
 
 // Destructor
-GateScannerSystem::~GateScannerSystem() 
+GateScannerSystem::~GateScannerSystem()
 {
   delete m_messenger;
 }
