33ff7f9ac50d35468bad012b329bb8bbd6d009d6
jawka <jakubbaran92@gmail.com>
2021-03-23 00:50:13 +0100

    pointer issue solved

--- source/digits_hits/include/GateRestrictedVolumeMessenger.hh
+++ source/digits_hits/include/GateRestrictedVolumeMessenger.hh
@@ -5,30 +5,36 @@
   See LICENSE.md for further details
   ----------------------*/
 
-
 #ifndef GateRestrictedVolumeMessenger_h
 #define GateRestrictedVolumeMessenger_h 1
 
 #include "GatePulseProcessorMessenger.hh"
+#include <memory>
 
 class G4UIcmdWithAString;
 
 class GateRestrictedVolume;
 
-class GateRestrictedVolumeMessenger: public GatePulseProcessorMessenger
+/** Author: Jakub Baran
+ *  Email: jakubbaran92@gmail.com
+ *  Organization: J-PET (http://koza.if.uj.edu.pl/pet/)
+ *  About class: Messenger class of the GateRestrictedVolume
+ **/
+
+class GateRestrictedVolumeMessenger : public GatePulseProcessorMessenger
 {
-  public:
-    GateRestrictedVolumeMessenger(GateRestrictedVolume* itsRestrictedVolume);
-    virtual ~GateRestrictedVolumeMessenger();
+public:
+  GateRestrictedVolumeMessenger(GateRestrictedVolume* itsRestrictedVolume);
 
-    inline void SetNewValue(G4UIcommand* aCommand, G4String aString);
+  inline void SetNewValue(G4UIcommand* aCommand, G4String aString);
 
-    inline GateRestrictedVolume* RestrictedVolume()
-      { return (GateRestrictedVolume*) GetPulseProcessor(); }
+  inline GateRestrictedVolume* RestrictedVolume() { return (GateRestrictedVolume*)GetPulseProcessor(); }
 
-  private:
-    G4UIcmdWithAString *restrictVolCmd = nullptr;
-    G4UIcmdWithAString *restrictModeCmd = nullptr;
+private:
+  G4String kAccept = "accept"; //!< Accept restriction mode string
+  G4String kReject = "reject"; //!< Reject restriction mode string
+  std::unique_ptr<G4UIcmdWithAString> restrictVolCmd;
+  std::unique_ptr<G4UIcmdWithAString> restrictModeCmd;
 };
 
 #endif
--- source/digits_hits/src/GateRestrictedVolumeMessenger.cc
+++ source/digits_hits/src/GateRestrictedVolumeMessenger.cc
@@ -5,7 +5,6 @@
   See LICENSE.md for further details
   ----------------------*/
 
-
 #include "GateRestrictedVolumeMessenger.hh"
 
 #include "GateRestrictedVolume.hh"
@@ -17,42 +16,39 @@ GateRestrictedVolumeMessenger::GateRestrictedVolumeMessenger(GateRestrictedVolum
 {
   G4String guidance;
   G4String cmdName;
-  G4String cmdName2;
 
   cmdName = GetDirectoryName() + "setRestrictedVolume";
-  restrictVolCmd = new G4UIcmdWithAString(cmdName,this);
+  restrictVolCmd.reset(new G4UIcmdWithAString(cmdName, this));
   restrictVolCmd->SetGuidance("Set restricted volume for pulse-discrimination");
 
-  cmdName2 = GetDirectoryName() + "setRestrictionMode";
-  restrictModeCmd = new G4UIcmdWithAString(cmdName2,this);
+  cmdName = GetDirectoryName() + "setRestrictionMode";
+  restrictModeCmd.reset(new G4UIcmdWithAString(cmdName, this));
   restrictModeCmd->SetGuidance("Set restriction mode, two are available: reject accept");
-
-}
-
-GateRestrictedVolumeMessenger::~GateRestrictedVolumeMessenger()
-{
-  delete restrictVolCmd;
-  delete restrictModeCmd;
 }
 
 void GateRestrictedVolumeMessenger::SetNewValue(G4UIcommand* command, G4String newValue)
 {
-  if (command==restrictVolCmd) { RestrictedVolume()->SetRestrictedVolume(newValue); }
-  
-  else if (command==restrictModeCmd) { 
-  
-    if (newValue == "accept" || newValue == "reject")
+  if (command == restrictVolCmd.get())
+  {
+    RestrictedVolume()->SetRestrictedVolume(newValue);
+  }
+
+  else if (command == restrictModeCmd.get())
+  {
+
+    if (newValue == kAccept || newValue == kReject)
       RestrictedVolume()->SetRestrictionMode(newValue);
-       
-    else {
+
+    else
+    {
       G4cerr << "No match for '" << newValue << "' restriction mode.\n";
       G4cerr << "Candidates are: reject accept\n";
-      
+
       G4String msg = "You must choose a restriction mode properly from two options: accept reject\n";
-      G4Exception( "GateRestrictedVolume::SetNewValue", "ProcessOnePulse", FatalException, msg);
-    }  
+      G4Exception("GateRestrictedVolume::SetNewValue", "ProcessOnePulse", FatalException, msg);
+    }
   }
-  
-  else 
-    GatePulseProcessorMessenger::SetNewValue(command,newValue);
+
+  else
+    GatePulseProcessorMessenger::SetNewValue(command, newValue);
 }
